01cdb6212a0aacaa9ac9cd27898dc2762ad52986
amiko <amikchixladze1234@gmail.com>
2023-06-22 18:17:43 +0400

    add animation 404 page

--- src/pages/404/Error404.jsx
+++ src/pages/404/Error404.jsx
@@ -1,17 +1,28 @@
 import "./Error404.scss";
 import ErrorImage from "../../assets/404error.png";
 import {Link} from "react-router-dom";
+import {motion} from "framer-motion";
 
 export default function Error404() {
    return (
       <>
-         <div className="error-404">
-            <div className="error-image">
+         <div className="error-404" style={{overflow:"hidden"}}>
+            <motion.div
+               initial={{x:-250,scale:0.6}}
+               animate={{x:0,scale:1}}
+               transition={{duration:1,type:"spring"}}
+               className="error-image"
+            >
                <img src={ErrorImage} alt="error image"/>
-            </div>
-            <div className="error-404-btn">
+            </motion.div>
+            <motion.div
+               initial={{x:150,scale:0.6}}
+               animate={{x:0,scale:1}}
+               transition={{duration:1,type:"spring"}}
+               className="error-404-btn"
+            >
                <Link to={"/"}>return home page</Link>
-            </div>
+            </motion.div>
          </div>
       </>
    );
